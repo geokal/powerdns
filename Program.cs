6038d9af1f7724e56fdc3f9c33cd81159f35546a
geokal <kalpgiorgos@gmail.com>
2023-10-19 21:56:02 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PowerDns.Client;
+using PowerDns.Client.Models;
+
+var client = new PowerDnsClient(
+    uri: new Uri("pdns:8081"), // without /api/v1
+    apiKey: "changeme");
+
+var zonesEndpoint = client.Servers["localhost"].Zones;
+
+List<Zone> zones = await zonesEndpoint.ReadAllAsync();
+
+RecordSet recordSet = await zonesEndpoint["example.org"].GetRecordSetAsync("www.example.org");
